bdfbb7a2c1ee49c39f00bd2552975232834868fd
Estelle TIEBERGHIEN <etieberg@e2r12p21.42.fr>
2019-02-27 20:10:19 +0100

    start level06

--- level06/level06.php
+++ level06/level06.php
@@ -12,7 +12,8 @@ function x($y, $z)
 $a = file_get_contents($y);
 $a = preg_replace("/(\[x (.*)\])/e", "y(\"\\2\")", $a);
 $a = preg_replace("/\[/", "(", $a);
-$a = preg_replace("/\]/", ")", $a); return $a;
+$a = preg_replace("/\]/", ")", $a);
+return $a;
 }
 
 $r = x($argv[1], $argv[2]); print $r;
